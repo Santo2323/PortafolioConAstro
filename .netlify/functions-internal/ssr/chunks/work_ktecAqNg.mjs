2c8076b046082ff51fbb8a87e802236314f96dfe
Santiago Beron <santiagobdea@gmail.com>
2024-01-05 20:10:50 -0300

    changes to title

--- .netlify/functions-internal/ssr/chunks/work_ktecAqNg.mjs
+++ .netlify/functions-internal/ssr/chunks/work_ktecAqNg.mjs
@@ -1,6 +1,6 @@
 export { renderers } from '../renderers.mjs';
 export { onRequest } from '../_empty-middleware.mjs';
 
-const page = () => import('./pages/work_MxVB_EAq.mjs');
+const page = () => import('./pages/work_9vsquwB-.mjs');
 
 export { page };
--- .netlify/functions-internal/ssr/entry.mjs
+++ .netlify/functions-internal/ssr/entry.mjs
@@ -1,12 +1,12 @@
 import * as adapter from '@astrojs/netlify/ssr-function.js';
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_S5k9BTPZ.mjs';
+import { manifest } from './manifest_gH0U2uDv.mjs';
 
 const _page0  = () => import('./chunks/generic_apJZ3nfd.mjs');
 const _page1  = () => import('./chunks/index_5V-fKriY.mjs');
 const _page2  = () => import('./chunks/about_3pzZT1tF.mjs');
 const _page3  = () => import('./chunks/_.._C-SFglpf.mjs');
-const _page4  = () => import('./chunks/work_ihK5KCOR.mjs');
+const _page4  = () => import('./chunks/work_ktecAqNg.mjs');
 const _page5  = () => import('./chunks/404_iuVZjU1L.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/about.astro", _page2],["src/pages/work/[...slug].astro", _page3],["src/pages/work.astro", _page4],["src/pages/404.astro", _page5]]);
 const _manifest = Object.assign(manifest, {
 	pageMap,

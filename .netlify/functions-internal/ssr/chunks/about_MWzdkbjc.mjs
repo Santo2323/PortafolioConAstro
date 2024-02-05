300cd09e1719a780b53ca5effa7cee49435ab1b6
Santiago Beron <santiagobdea@gmail.com>
2024-02-05 18:17:54 -0300

    comit

--- .netlify/functions-internal/ssr/chunks/about_MWzdkbjc.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/about_FsrbJ-S2.mjs').then(n => n.b);
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/work_rqFIv2PE.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/work_qB2hG3GI.mjs');
-
-export { page };
